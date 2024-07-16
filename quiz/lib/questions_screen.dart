7f060e104c2d184b05a218a16c3d08780bfa7faa
Matthew Serafica <matthewserafica01@gmail.com>
2024-07-17 03:06:13 +0800

    Update questions_screen added layout for the questions and answers

--- quiz/lib/questions_screen.dart
+++ quiz/lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -9,9 +10,42 @@ class QuestionsScreen extends StatefulWidget {
   }
 }
 
+void test() {
+  //...
+}
+
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('QuestionScreen');
+    return const SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'The question...',
+            style: TextStyle(
+              color: Color.fromARGB(255, 237, 223, 252),
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          AnswerButton(
+            answerText: 'Answer 1',
+            onTap: test,
+          ),
+          AnswerButton(
+            answerText: 'Answer 2',
+            onTap: test,
+          ),
+          AnswerButton(
+            answerText: 'Answer 3',
+            onTap: test,
+          ),
+        ],
+      ),
+    );
   }
 }
