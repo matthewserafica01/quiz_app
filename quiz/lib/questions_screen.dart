ba7518e336c5c530af09398e9d6fa4bc754f6460
Matthew Serafica <matthewserafica01@gmail.com>
2024-07-18 22:55:03 +0800

    update questions_screen added questions and answers for the question screen widget

--- quiz/lib/questions_screen.dart
+++ quiz/lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz/answer_button.dart';
+import 'package:quiz/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -17,31 +18,37 @@ void test() {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const SizedBox(
+    final currentQuestion = questions[0];
+
+    return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            'The question...',
-            style: TextStyle(
+            currentQuestion.text,
+            style: const TextStyle(
               color: Color.fromARGB(255, 237, 223, 252),
-              fontSize: 18,
+              fontSize: 14,
             ),
           ),
-          SizedBox(
+          const SizedBox(
             height: 30,
           ),
           AnswerButton(
-            answerText: 'Answer 1',
+            answerText: currentQuestion.answers[0],
             onTap: test,
           ),
           AnswerButton(
-            answerText: 'Answer 2',
+            answerText: currentQuestion.answers[1],
             onTap: test,
           ),
           AnswerButton(
-            answerText: 'Answer 3',
+            answerText: currentQuestion.answers[2],
+            onTap: test,
+          ),
+          AnswerButton(
+            answerText: currentQuestion.answers[3],
             onTap: test,
           ),
         ],
